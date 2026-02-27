36d90063c0d983762d9803bd4d487a2307a7f5bd
Mohammedmagharbeh <magharbehmohammed@gmail.com>
2026-02-28 00:07:12 +0300

    Refactor code structure for improved readability and maintainability

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,16 +1,43 @@
+// import i18n from "i18next";
+// import { initReactI18next } from "react-i18next";
+// import LanguageDetector from "i18next-browser-languagedetector";
+// import Backend from "i18next-http-backend";
+
+// i18n
+//   .use(Backend) // load translations from /public/locales
+//   .use(LanguageDetector)
+//   .use(initReactI18next)
+//   .init({
+//     fallbackLng: "en",
+//     supportedLngs: ["en", "ar"],
+//     debug: false,
+//     detection: {
+//       order: ["localStorage", "navigator", "htmlTag"],
+//       caches: ["localStorage"],
+//     },
+//     interpolation: {
+//       escapeValue: false,
+//     },
+//   });
+
+// export default i18n;
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
-
 i18n
-  .use(Backend) // load translations from /public/locales
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
     supportedLngs: ["en", "ar"],
-    debug: false,
+    backend: {
+      // السلاش البادئ / ضروري جداً لأنه يشير لمجلد الببلك بعد الـ Build
+backend: {
+  // السلاش في البداية يعني "ابحث في المجلد العام للمشروع"
+  loadPath: '/locales/{{lng}}.json', 
+},    },
     detection: {
       order: ["localStorage", "navigator", "htmlTag"],
       caches: ["localStorage"],
@@ -19,5 +46,3 @@ i18n
       escapeValue: false,
     },
   });
-
-export default i18n;
